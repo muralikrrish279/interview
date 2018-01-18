2ca0c72f140a056fc15983052341509534b3c6fb
Muralikrishnan Anandakumar <muralikrrish279@gmail.com>
2018-01-18 15:19:57 -0800

    rearrange zeros problem

---
+++ CrackingBook/Arrays/RearrangeZeros.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+class RearrangeZeros
+{
+    // static void Main(string[] args)
+    // {
+    //     //RearrangeZerosEnd(new int[] { 0, 1, 0, 2, 0, 4, 5, 6 });
+    //     RearrangeZerosFront(new int[] { 0, 1, 0, 2, 0, 4, 5, 6 });
+    //     //RearrangeZerosPositivesNegatives(new int[] { -1,2,3,-4,8,-3,19,-6});
+    // }
+
+    static void RearrangeZerosEnd(int[] numbers)
+    {
+        int count = 0;
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] != 0)
+            {
+                numbers[count] = numbers[i];
+                count++;
+            }
+        }
+
+        while (count < numbers.Length)
+        {
+            numbers[count] = 0;
+            count++;
+        }
+
+        foreach (var item in numbers)
+        {
+            Console.WriteLine(item);
+        }     
+    }
+
+       static void RearrangeZerosFront(int[] numbers)
+    {
+        int count = numbers.Length - 1;
+
+        for (int i = numbers.Length -1; i >= 0; i--)
+        {
+            if (numbers[i] != 0)
+            {
+                numbers[count] = numbers[i];
+                count--;
+            }
+        }
+
+        while (count >= 0 )
+        {
+            numbers[count] = 0;
+            count--;
+        }
+
+        foreach (var item in numbers)
+        {
+            Console.WriteLine(item);
+        }     
+    }
+
+     static void RearrangeZerosPositivesNegatives(int[] numbers)
+    {
+        int negativeCount = 0;
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] < 0)
+            {
+                var temp = numbers[negativeCount];
+                numbers[negativeCount] = numbers[i]; 
+                numbers[i] = temp;
+                negativeCount++;
+            }
+        }
+
+        foreach (var item in numbers)
+        {
+            Console.WriteLine(item);
+        }     
+    }
+
+
+
+}
+
+
