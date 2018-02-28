b7bccfd41a754f387a20dccbde22a15f7dabd2c7
Muralikrishnan Anandakumar <v-muanan@microsoft.com>
2018-02-27 17:04:39 -0800

    cleanup

--- CrackingBook/Program.cs
+++ CrackingBook/Program.cs
@@ -16,29 +16,7 @@ namespace CrackingBook
     {
 
 
-        static void Main(string[] args)
-        {
-            var path = Environment.CurrentDirectory.Replace(@"bin\Debug", "");
-            var count = 0;
-
-            ListCount(path, ref count);
-        }
-
-        static void ListCount(string path, ref int count)
-        {
-
-            var dire = new System.IO.DirectoryInfo(path);
-
-            foreach (var item in dire.GetDirectories())
-            {
-                ListCount(item.FullName, ref count);
-            }
-            foreach (var item in dire.GetFiles("*.cs"))
-            {
-                count++;
-                Console.WriteLine(count + " : " + item.Directory.Name + " - " + item.Name);
-            }
-        }
+        
         //static void Main(string[] args)
         //{
 
