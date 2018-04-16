401432db4a49673aa462e857bc5f90889b1dfdae
muralikrrish279 <muralikrrrish279@gmail.com>
2018-04-15 20:34:40 -0700

    changes

--- CrackingBook/Trees/CheckBst.cs
+++ CrackingBook/Trees/CheckBst.cs
@@ -3,30 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CrackingBook.Utils;
 
-namespace CrackingBook.Trees
+namespace CrackingBook
 {
     public class CheckBst
     {
-        public class Node
+
+        public static bool checkBST(TreeNode<int> root)
         {
-            public int data;
-            public Node left;
-            public Node right;
+            return Check(root, null, null);
         }
 
-        public bool checkBST(CheckBst.Node root)
-        {
-            return Check(root, 0, 10000);
-        }
-
-        private bool Check(Node n, int min, int max)
+        private  static bool Check(TreeNode<int> n, int? min, int? max)
         {
             if (n == null)
             {
                 return true;
             }
-            else if (n.data <= min || n.data >= max)
+            else if (( min != null && n.data <= min) || (max != null && n.data >= max))
             {
                 return false;
             }
@@ -35,5 +30,20 @@ namespace CrackingBook.Trees
                 return Check(n.left, min, n.data) && Check(n.right, n.data, max);
             }
         }
-    }
+
+		//static void Main(string[] args)
+		//{
+
+		//    var root = new TreeNode<int>(8);
+		//    root.left = new TreeNode<int>(3);
+		//    root.right = new TreeNode<int>(10);
+		//    root.left.left = new TreeNode<int>(1);
+		//    root.left.right = new TreeNode<int>(6);
+		//    root.left.right.left = new TreeNode<int>(4);
+		//    root.left.right.right = new TreeNode<int>(7);
+		//    root.right.right = new TreeNode<int>(14);
+		//    root.right.left = new TreeNode<int>(9);
+  //          Console.Write(checkBST(root));
+		//}
+	}
 }
--- CrackingBook/Trees/ListOfDepths.cs
+++ CrackingBook/Trees/ListOfDepths.cs
@@ -41,19 +41,19 @@ namespace CrackingBook
             Console.Read();
         }
 
-        static void Main(string[] args)
-        {
+        //static void Main(string[] args)
+        //{
 
-            var root = new TreeNode<int>(8);
-            root.left = new TreeNode<int>(3);
-            root.right = new TreeNode<int>(10);
-            root.left.left = new TreeNode<int>(1);
-            root.left.right = new TreeNode<int>(6);
-            root.left.right.left = new TreeNode<int>(4);
-            root.left.right.right = new TreeNode<int>(7);
-            root.right.right = new TreeNode<int>(14);
-            root.right.left = new TreeNode<int>(13);
-            FindDepth(root,new Dictionary<int, List<int>>(),0);
-        }
+        //    var root = new TreeNode<int>(8);
+        //    root.left = new TreeNode<int>(3);
+        //    root.right = new TreeNode<int>(10);
+        //    root.left.left = new TreeNode<int>(1);
+        //    root.left.right = new TreeNode<int>(6);
+        //    root.left.right.left = new TreeNode<int>(4);
+        //    root.left.right.right = new TreeNode<int>(7);
+        //    root.right.right = new TreeNode<int>(14);
+        //    root.right.left = new TreeNode<int>(13);
+        //    FindDepth(root,new Dictionary<int, List<int>>(),0);
+        //}
     }
 }
