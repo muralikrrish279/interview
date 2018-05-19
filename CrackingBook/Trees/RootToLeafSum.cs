a51e85c69815e1335935f621085f885f520c81e2
muralikrrish279 <muralikrrrish279@gmail.com>
2018-05-19 11:40:16 +0530

    Corecting files

--- CrackingBook/Trees/RootToLeafSum.cs
+++ CrackingBook/Trees/RootToLeafSum.cs
@@ -8,37 +8,25 @@ using CrackingBook.Utils;
 namespace CrackingBook
 {
     //Expedia phone round question
-    public class IsSubTree
+    public class RootToLeafSum
     {
-        public static bool CheckSubTree(TreeNode<int> node1, TreeNode<int> node2)
-        {
-            if (node1 == null)
-            {
-                return false;
-            }
-            if (node1.data == node2.data && MAtchTree(node1, node2))
-            {
-                return true;
-            }
-            return CheckSubTree(node1.left, node2) || CheckSubTree(node1.right, node2);
-        }
+        
 
-        public static bool MAtchTree(TreeNode<int> node1, TreeNode<int> node2)
+        public static void FindSum(TreeNode<int> node1, int currentSum)
         {
-            if (node1 == null && node2 == null) {
-                return true;
-            }
-            if (node1 == null || node2 == null)
-            {
-                return false;
-            }
-            if (node1.data != node2.data)
-            {
-                return false;
-            }
-            return MAtchTree(node1.left, node2.left) && MAtchTree(node1.right, node2.right);
-        }
+			if(node1 == null){
+				return;
+			}
 
+			currentSum += node1.data;
+			if(node1.left == null && node1.right == null){
+				Console.WriteLine(currentSum);
+			}
+
+			FindSum(node1.left, currentSum);
+			FindSum(node1.right, currentSum);
+        }
+        
         //static void Main(string[] args)
         //{
 
@@ -50,11 +38,7 @@ namespace CrackingBook
         //    root.right.left = new TreeNode<int>(6);
         //    root.right.right = new TreeNode<int>(9);
 
-       
-        //    var node1 = new TreeNode<int>(7);
-        //    node1.left = new TreeNode<int>(6);
-        //    node1.right = new TreeNode<int>(10);
-        //    Console.WriteLine(CheckSubTree(root, node1));
+        //    FindSum(root, 0);
         //}
     }
 }
