d201f1cc28637435c5c0db068f3cee3f273df8c7
muralikrrish279 <muralikrrrish279@gmail.com>
2018-05-21 07:33:14 +0530

    Target sum

--- CrackingBook/Trees/RootToLeafSum.cs
+++ CrackingBook/Trees/RootToLeafSum.cs
@@ -8,37 +8,61 @@ using CrackingBook.Utils;
 namespace CrackingBook
 {
     //Expedia phone round question
-    public class RootToLeafSum
+    public class TargetSum
     {
         
 
-        public static void FindSum(TreeNode<int> node1, int currentSum)
+        public static int Find1Sum(TreeNode<int> node1, int currentSum,int targetSum,Dictionary<int,int> count)
         {
 			if(node1 == null){
-				return;
+				return 0;
 			}
 
 			currentSum += node1.data;
-			if(node1.left == null && node1.right == null){
-				Console.WriteLine(currentSum);
+			int sum = currentSum - targetSum;
+			int totalPaths = 0;
+			count.TryGetValue(sum, out totalPaths);
+
+			if(currentSum == targetSum){
+				totalPaths++;
 			}
 
-			FindSum(node1.left, currentSum);
-			FindSum(node1.right, currentSum);
+			manageHshtable(currentSum, 1, count);
+			totalPaths += Find1Sum(node1.left, currentSum, targetSum, count);
+			totalPaths += Find1Sum(node1.right, currentSum, targetSum, count);
+			manageHshtable(currentSum, -1, count);
+			return totalPaths;
         }
         
-        //static void Main(string[] args)
-        //{
+		public static void manageHshtable( int key, int delat, Dictionary<int, int> count)
+        {
+			var newcount = 0;
 
-        //    var root = new TreeNode<int>(4);
-        //    root.left = new TreeNode<int>(2);
-        //    root.right = new TreeNode<int>(7);
-        //    root.left.left = new TreeNode<int>(1);
-        //    root.left.right = new TreeNode<int>(3);
-        //    root.right.left = new TreeNode<int>(6);
-        //    root.right.right = new TreeNode<int>(9);
+			count.TryGetValue(key, out newcount); 
+			newcount += delat;
+			if(newcount ==0 && count.ContainsKey(key)){
+				count.Remove(key);
+			}else{
+				count.Add(key,newcount);
+			}
+        }
+        
+   //     static void Main(string[] args)
+   //     {
 
-        //    FindSum(root, 0);
+   //         var root = new TreeNode<int>(10);
+   //         root.left = new TreeNode<int>(5);
+   //         root.right = new TreeNode<int>(-3);
+			//root.right.right = new TreeNode<int>(11);
+   //         root.left.left = new TreeNode<int>(3);
+   //         root.left.left.left = new TreeNode<int>(3);
+			//root.left.left.right = new TreeNode<int>(-2);
+			//root.left.right = new TreeNode<int>(1);
+			//root.left.right.right = new TreeNode<int>(2);
+
+            
+   //         var output = Find1Sum(root, 0,8,new Dictionary<int, int>());
+			//Console.WriteLine(output);
         //}
     }
 }
---
+++ CrackingBook/Trees/TargetSum.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CrackingBook.Utils;
+
+namespace CrackingBook
+{
+    //Expedia phone round question
+    public class RootToLeafSum
+    {
+        
+
+        public static void FindSum(TreeNode<int> node1, int currentSum)
+        {
+			if(node1 == null){
+				return;
+			}
+
+			currentSum += node1.data;
+			if(node1.left == null && node1.right == null){
+				Console.WriteLine(currentSum);
+			}
+
+			FindSum(node1.left, currentSum);
+			FindSum(node1.right, currentSum);
+        }
+        
+        //static void Main(string[] args)
+        //{
+
+        //    var root = new TreeNode<int>(4);
+        //    root.left = new TreeNode<int>(2);
+        //    root.right = new TreeNode<int>(7);
+        //    root.left.left = new TreeNode<int>(1);
+        //    root.left.right = new TreeNode<int>(3);
+        //    root.right.left = new TreeNode<int>(6);
+        //    root.right.right = new TreeNode<int>(9);
+
+        //    FindSum(root, 0);
+        //}
+    }
+}
