86cb1e2c19b7e32458f5d032ea1b8ac452a7f88e
Muralikrishnan Anandakumar <v-muanan@microsoft.com>
2018-12-17 12:50:24 -0800

    adding more problems

---
+++ CrackingBook/DynamicProgramming/OverlappingRects.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrackingBook
+{
+    public class Point
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    //https://www.geeksforgeeks.org/total-area-two-overlapping-rectangles/
+    public class OverlappingRects
+    {
+
+        public static int TotalArea(Point L1, Point R1, Point L2, Point R2)
+        {
+            int width = Math.Min(R2.x, R1.x) - Math.Max(L2.x, L1.x);
+            int length = Math.Min(R2.y, R1.y) - Math.Max(L2.y, L1.y);
+            if (width < 0 || length < 0)
+            {
+                return -1;
+            }
+            return width * length;
+        }
+
+
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine(TotalArea(new Point(2, 2), new Point(5, 7), new Point(3, 4), new Point(6, 9)));
+        //}
+    }
+}
---
+++ CrackingBook/Trees/MorrisTraversal.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CrackingBook.Utils;
+
+namespace CrackingBook.Trees
+{
+    class MorrisTraversal
+    {
+
+        static void MorrisTraversal1(TreeNode<int> root)
+        {
+            TreeNode<int> current, pre;
+
+            if (root == null)
+                return;
+
+            current = root;
+            while (current != null)
+            {
+                if (current.left == null)
+                {
+                    Console.WriteLine(current.data + " ");
+                    current = current.right;
+                }
+                else
+                {
+                    /* Find the inorder predecessor of current */
+                    pre = current.left;
+                    while (pre.right != null && pre.right != current)
+                        pre = pre.right;
+
+                    /* Make current as right child of its inorder predecessor */
+                    if (pre.right == null)
+                    {
+                        pre.right = current;
+                        current = current.left;
+                    }
+
+                    /* Revert the changes made in if part to restore the  
+                        original tree i.e., fix the right child of predecssor*/
+                    else
+                    {
+                        pre.right = null;
+                        Console.WriteLine(current.data + " ");
+                        current = current.right;
+                    } /* End of if condition pre->right == NULL */
+
+                } /* End of if condition current->left == NULL*/
+
+            } /* End of while */
+        }
+
+        //public static void Main()
+        //{
+        //    /* Constructed binary tree is 
+        //           1 
+        //         /   \ 
+        //        2      3 
+        //      /  \ 
+        //    4     5 
+        //    */
+        //    BinaryTree<int> tree = new BinaryTree<int>();
+        //    tree.Root = new TreeNode<int>(1);
+        //    tree.Root.left = new TreeNode<int>(2);
+        //    tree.Root.right = new TreeNode<int>(3);
+        //    tree.Root.left.left = new TreeNode<int>(4);
+        //    tree.Root.left.right = new TreeNode<int>(5);
+
+        //    MorrisTraversal1(tree.Root);
+        //}
+    }
+}
