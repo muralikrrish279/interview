32d29530e33cd43e6167960ebee0d397af015eaa
Muralikrishnan Anandakumar <v-muanan@microsoft.com>
2018-12-18 21:30:08 -0800

    adding min heap

--- CrackingBook/Practice1.cs
+++ CrackingBook/Practice1.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CrackingBook.Utils;
 
 namespace CrackingBook
 {
     public class Practice1
     {
-       
+
         //static void Main(string[] args)
         //{
         //    //prodArray(new int[] { 1, 2, 3, 4 });
@@ -16,7 +17,15 @@ namespace CrackingBook
         //    //         new int[]{10,20,30},
         //    //         new int[]{15,25,35},
         //    //         new int[]{16,26,36}});
-        //    replacezeros(new int[] { 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1 });
+        //    var heap = new MinHeap();
+        //    heap.push(10);
+        //    heap.push(5);
+        //    heap.push(15);
+        //    heap.push(20);
+        //    heap.push(8);
+        //    Console.WriteLine(heap.peek());
+        //    heap.poll();
+        //    Console.WriteLine(heap.peek());
         //}
     }
 }
---
+++ CrackingBook/Utils/MinHeap.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrackingBook.Utils
+{
+    public class MinHeap
+    {
+        public MinHeap()
+        {
+
+        }
+
+        public int size = 0;
+        int Capacity = 10;
+        private int[] arr = new int[10];
+
+        private int LeftChildIndex(int index) {
+            return index * 2 + 1;
+        }
+
+        private void Swap(int leftIndex, int rightIndex) {
+            var temp = arr[leftIndex];
+            arr[leftIndex] = arr[rightIndex];
+            arr[rightIndex] = temp;
+        }
+
+        private int RightChildIndex(int index)
+        {
+            return index * 2 + 2;
+        }
+
+        private int ParentIndex(int index)
+        {
+            return (index -2)/2;
+        }
+
+        public int peek() {
+            if (this.size < 0)
+                throw new InvalidOperationException();
+            return arr[0];
+        }
+
+        public int poll()
+        {
+            if (this.size < 0)
+                throw new InvalidOperationException();
+            var item = arr[0];
+            size--;
+            Swap(0, size);
+            HeapifyDown();           
+            return item;
+        }
+
+        public int push(int item)
+        {
+            if (this.size < 0)
+                throw new InvalidOperationException();
+            arr[size] = item;
+            size++;
+            HeapifyUp();
+            return item;
+        }
+
+
+        public void HeapifyDown() {
+            var index = 0;
+            while (LeftChildIndex(index) < size) {
+                int leftIndex = LeftChildIndex(index);
+                int rightIndex = RightChildIndex(index);
+                int smalledIndex = leftIndex;
+                if (rightIndex < size && arr[leftIndex] > arr[rightIndex])
+                {
+                    smalledIndex = rightIndex;
+                }
+                if (arr[index] == arr[smalledIndex])
+                {
+                    break;
+                }
+                else {
+                    Swap(index, smalledIndex);
+                    index = smalledIndex;
+                }
+            }
+        }
+
+        public void HeapifyUp()
+        {
+            var index = size - 1;
+            while (ParentIndex(index) >= 0 && arr[index] < arr[ParentIndex(index)]) {
+                Swap(ParentIndex(index), index);
+                index = ParentIndex(index);
+            }
+        }
+    }
+}
